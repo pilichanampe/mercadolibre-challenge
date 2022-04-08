efe374bf949a0c3bd4188f1b9a673b94c765c7bc
Pili Chanampe <mariadelpilarchanampe@gmail.com>
2022-04-07 23:50:02 -0300

    favicon and title added in index.html, image added in root

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Container } from './components/basecomponents/Container';
+import { Image } from './components/basecomponents/Image';
 import Loading from './components/basecomponents/Loading';
 import { Span1 } from './components/basecomponents/Text';
 import { useProductsContext } from './context/ProductsContext';
+import Meli from './assets/meli.png';
 
 function App() {
   const { loading } = useProductsContext();
@@ -15,7 +17,17 @@ function App() {
       }
       {
         !loading &&
-        <Span1>¡Te damos la bienvenida a Mercado Libre Argentina! Buscá ese producto que querés :).</Span1>
+        <Container
+          flexDirection="column"
+        >
+          <Span1>¡Te damos la bienvenida a Mercado Libre Argentina! Buscá ese producto que querés :).</Span1>
+          <Image
+            alt="Imagen de Mercado Libre"
+            mt="100px" 
+            src={Meli}
+          />
+
+        </Container>
       }
     </Container>
   )
